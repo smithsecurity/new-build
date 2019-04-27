fa78886d68abfabbedf2b5a37c85a906a5f68f2b
smithsecurity <46826865+smithsecurity@users.noreply.github.com>
2019-04-27 08:53:15 -0400

    Added sublist3r.

--- new-build-tools.py
+++ new-build-tools.py
@@ -1,7 +1,8 @@
-# Run on a newly built kali machine to quickly download
+ Run on a newly built kali machine to quickly download
 # and install the following tools:
 # Empire
 # CrackMapExec
+# Sublist3r
 # Gobuster
 # Terminator
 # Bloodhound
@@ -22,11 +23,21 @@ print("Done.")
 print("Downloading CrackMapExec.")
 subprocess.call(["git", "clone", "--recursive", "https://github.com/byt3bl33d3r/CrackMapExec.git"])
 print("Done.")
+
 print("Installing CrackMapExec")
 os.chdir('CrackMapExec/')
 subprocess.call(["./setup.py", "install"])
+
 print("Running cme first time setup")
 subprocess.call(["cme"])
+
+print("Downloading sublist3r.")
+subprocess.call(["git", "clone", "https://github.com/aboul3la/Sublist3r.git"])
+print("Done.")
+
+print("Installing sublist3r.")
+subprocess.call(["pip", "install", "-r" "sublist3r/requirements.txt"])
+print("Done.")
 
 print("Downloading gobuster.")
 subprocess.call(["apt-get", "install", "gobuster", "-y"])
